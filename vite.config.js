4567d4a71c2ac461a93a0eed43a5663440b59734
Aryan Soni <aryansoni1128@gmail.com>
2025-10-28 04:50:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ideathon/', // Add this line for GitHub Pages
+  base: './', // Change to this for GitHub Pages
 })
