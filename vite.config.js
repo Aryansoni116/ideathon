5da7ec2975d04ca45abdd82176854c9e5d56c3f5
Aryan Soni <aryansoni1128@gmail.com>
2025-10-28 08:50:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    // Copy public files to dist
+    copyPublicDir: true
+  },
+  publicDir: 'public'
 })
